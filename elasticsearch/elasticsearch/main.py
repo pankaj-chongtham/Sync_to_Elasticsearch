0cb27db8dd6c91341f07fed10a3cd12c96a9fbde
14791@ramco.com <14791@ramco.com>
2023-11-30 14:45:16 +0530

    create index method is added

--- elasticsearch/elasticsearch/main.py
+++ elasticsearch/elasticsearch/main.py
@@ -2,3 +2,6 @@ from elasticsearch import Elasticsearch
 
 es=Elasticsearch("http://localhost:9200")
 print(es.ping())
+
+def create_index():
+    return "This will create the index in Elasticsearch"
