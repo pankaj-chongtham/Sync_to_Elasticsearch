aa4de3a3ab1338c9ad92ce5d919af8ca535d35b0
cs-pjt <lumos.complex8@gmail.com>
2023-12-04 16:43:55 +0530

    test

--- elasticsearch/elasticsearch/main.py
+++ elasticsearch/elasticsearch/main.py
@@ -3,5 +3,4 @@ from elasticsearch import Elasticsearch
 es=Elasticsearch("http://localhost:9200")
 print(es.ping())
 
-def create_index():
-    return "This will create the index in Elasticsearch"
+#trial comment
