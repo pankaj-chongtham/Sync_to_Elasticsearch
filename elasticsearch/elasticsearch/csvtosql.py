300c7e2c00a2f8ec68914497ddc4fe1236a2219d
Gnaneshwari-S-19 <97173315+Gnaneshwari-S-19@users.noreply.github.com>
2023-12-04 16:08:49 +0530

    csv sentences added to sql table

---
+++ elasticsearch/elasticsearch/csvtosql.py
@@ -0,0 +1,31 @@
+import csv
+import pyodbc
+
+# CSV file path
+csv_file_path = 'C:\\Users\\yuvag\\Downloads\\sentences_1000.csv'
+
+# Table details
+table_name = 'discrepancy'
+column_name = 'description'
+
+# Establish a connection to the database
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                        'SERVER=localhost\SQLEXPRESS;'
+                        'DATABASE=master;'
+                        'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+
+# Read data from CSV and insert into the SQL table
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for row in csv_reader:
+        insert_query = f"INSERT INTO {table_name} ({column_name}) VALUES (?)"
+
+        # Execute the INSERT statement with the value from the CSV
+        cursor.execute(insert_query, row[0])
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
