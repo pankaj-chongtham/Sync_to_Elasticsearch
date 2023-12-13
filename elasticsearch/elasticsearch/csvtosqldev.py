39c66ff107512d15b81d0c8fd87aa8ed7cb7498b
14791@ramco.com <14791@ramco.com>
2023-12-13 20:36:22 +0530

    insert query

--- elasticsearch/elasticsearch/csvtosqldev.py
+++ elasticsearch/elasticsearch/csvtosqldev.py
@@ -12,14 +12,28 @@ csv_file_path = 'C:\\Users\\yuvag\\Downloads\\master.csv'
 
 # SQL Server table name
 table_name = 'discrepancy'
-
 # Read CSV file into a DataFrame
 df = pd.read_csv(csv_file_path)
 
-# Insert data into SQL Server table
-df.to_sql(name=table_name, con=conn)
+def pandas_inbuilt():
+    # Insert data into SQL Server table
+    df.to_sql(name=table_name, con=conn)
 
-# Close the connection
-conn.close()
+    # Close the connection
+    conn.close()
 
-print(f'Data inserted into {table_name} table.')
+    print(f'Data inserted into {table_name} table.')
+
+
+def using_insert_statement():
+    column_names = "" # list out all column name here. For Example: "columnname1, columnname2 and so on" without quotes
+    for index, row in df.iterrows():
+        try:
+            insert_query = f"INSERT INTO {table_name} ({column_names}) VALUES (?,?);" # put no. of '?' on how many columns
+            cursor.execute(insert_query, tuple(row))
+        except Exception as e:
+            print(e)
+    conn.commit()
+    conn.close()
+
+using_insert_statement()
