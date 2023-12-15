64441f73d1affd7e2d4dbba27cf7f84e688e6215
cs-pjt <lumos.complex8@gmail.com>
2023-12-15 20:56:17 +0530

    cdc query using python

--- elasticsearch/elasticsearch/cdc.py
+++ elasticsearch/elasticsearch/cdc.py
@@ -1,4 +1,5 @@
 import pyodbc
+import pandas as pd
 
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
                         'SERVER=DESKTOP-EKJH7K7;'
@@ -7,18 +8,27 @@ conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
 
 cursor = conn.cursor()
 
-query = """
-SELECT * FROM cdc.fn_cdc_get_all_changes_dbo_discrepancy(
-    sys.fn_cdc_get_min_lsn('dbo_discrepancy', 'disc_cdc'), 
-    sys.fn_cdc_get_max_lsn(), 
-    'all'
-)
-"""
+cmn_df = pd.read_sql("select * from CMN_Elstc_index_tbl", con=conn)
+condition_records = cmn_df.to_dict(orient='records')
+for record in condition_records:
+    Elstc_sync_mode_name = record.get('Elstc_sync_mode')
+    print(Elstc_sync_mode_name)
 
-cursor.execute(query)
-rows = cursor.fetchall()
+if Elstc_sync_mode_name = 'cdc':
+    try:
+        __$start_lsn = slsn
+        query = """
+        USE checkcdc
+        DECLARE @from_lsn binary(10), @to_lsn binary(10)
+        SELECT @from_lsn = sys.fn_cdc_get_min_lsn('prime')
+        SELECT @to_lsn = sys.fn_cdc_get_max_lsn()
+        SELECT * FROM cdc.fn_cdc_get_all_changes_prime(@from_lsn, @to_lsn, 'all') where where slsn = 
+        """
 
-for row in rows:
-    print(row)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
 
 conn.close()
--- elasticsearch/elasticsearch/main.py
+++ elasticsearch/elasticsearch/main.py
@@ -1,6 +1,23 @@
+import pandas as pd
 from elasticsearch import Elasticsearch
 
 es=Elasticsearch("http://localhost:9200")
 print(es.ping())
 
 #trial comment
+
+def sync():
+
+    # read cmn_elsc_index_tbl to df
+    meta_df = pd.read_sql_query('selefct*from ')
+
+    # iterate meta_df
+    for index, row in meta_df.iterrows():
+        try:
+            # get table data
+            table_name = 'discrepancy'
+            table_df = pd.read_sql_query(f'select*from {table_name}')
+
+            #
+        except Exception e:
+            print(e)
